cbffd3e7b59eff397d312d58be7f50d6b17b95e2
MLewandovski <maksim.lewandovskiy@apriorit.com>
2024-06-10 22:54:37 +0300

    buf tricks

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+)
+
+type bytesClosingBuffer struct {
+	*bytes.Buffer
+	io.Closer
+}
+
+func newBytesClosingBuffer() *bytesClosingBuffer {
+	return &bytesClosingBuffer{
+		Buffer: new(bytes.Buffer),
+	}
+}
+
+func (b *bytesClosingBuffer) Close() error {
+	fmt.Println("Closing")
+	return nil
+}
+
+func writeTo(w io.WriteCloser, msg []byte) error {
+	defer w.Close()
+
+	_, err := w.Write(msg)
+	return err
+}
 
 func main() {
-	fmt.Print("Hello world")
+	buf := newBytesClosingBuffer()
+
+	err := writeTo(buf, []byte("New bytes"))
+	if err != nil {
+		log.Fatal("Errrrrr")
+		//close
+	}
+
+	fmt.Println(buf.String())
 }
